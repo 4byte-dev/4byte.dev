df901969033a744df72c885fbb163c5cbf0df939
Ömer Faruk Demirel <omerfdmrl@gmail.com>
2025-12-31 13:03:44 +0300

    test: update article factory test counts

--- modules/Article/tests/Feature/Database/Seeders/ArticleSeederTest.php
+++ modules/Article/tests/Feature/Database/Seeders/ArticleSeederTest.php
@@ -5,6 +5,10 @@ namespace Modules\Article\Tests\Feature\Database\Seeders;
 use Modules\Article\Database\Seeders\ArticleSeeder;
 use Modules\Article\Models\Article;
 use Modules\Article\Tests\TestCase;
+use Modules\React\Models\Comment;
+use Modules\React\Models\Dislike;
+use Modules\React\Models\Like;
+use Modules\React\Models\Save;
 
 class ArticleSeederTest extends TestCase
 {
@@ -12,6 +16,10 @@ class ArticleSeederTest extends TestCase
     {
         $this->seed(ArticleSeeder::class);
 
-        $this->assertDatabaseCount(Article::class, 20);
+        $this->assertDatabaseCount(Article::class, 5);
+        $this->assertDatabaseCount(Like::class, 15);
+        $this->assertDatabaseCount(Dislike::class, 15);
+        $this->assertDatabaseCount(Save::class, 15);
+        $this->assertDatabaseCount(Comment::class, 15);
     }
 }
