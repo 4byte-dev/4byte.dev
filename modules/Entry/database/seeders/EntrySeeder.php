3dbb8326e61fff43a4ab36d946b29b711cafd378
Ömer Faruk Demirel <omerfdmrl@gmail.com>
2025-12-31 10:28:40 +0300

    refactor: improvement for seeders and factories

--- modules/Entry/database/seeders/EntrySeeder.php
+++ modules/Entry/database/seeders/EntrySeeder.php
@@ -13,11 +13,12 @@ class EntrySeeder extends Seeder
 {
     public function run(): void
     {
-        Entry::factory(20)->create()->each(function (Entry $entry) {
-            Like::factory(3)->forModel($entry)->create();
-            Dislike::factory(3)->forModel($entry)->create();
-            Save::factory(3)->forModel($entry)->create();
-            Comment::factory(5)->forModel($entry)->create();
-        });
+        Entry::factory()
+            ->count(7)
+            ->has(Like::factory()->count(3), 'likes')
+            ->has(Dislike::factory()->count(3), 'dislikes')
+            ->has(Save::factory()->count(3), 'saves')
+            ->has(Comment::factory()->count(3), 'comments')
+            ->create();
     }
 }
