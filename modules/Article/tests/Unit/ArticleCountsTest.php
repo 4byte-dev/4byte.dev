e8cd4d3167d817821af93652b2f13d35612f691e
Ömer Faruk Demirel <omerfdmrl@gmail.com>
2026-01-13 00:03:09 +0300

    test(article): fix event tests

--- modules/Article/tests/Unit/ArticleCountsTest.php
+++ modules/Article/tests/Unit/ArticleCountsTest.php
@@ -4,7 +4,12 @@ namespace Modules\Article\Tests\Unit;
 
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\Event;
 use Modules\Article\Models\Article;
+use Modules\React\Events\UserDislikedEvent;
+use Modules\React\Events\UserLikedEvent;
+use Modules\React\Events\UserUndislikedEvent;
+use Modules\React\Events\UserUnlikedEvent;
 use Modules\React\Models\Comment;
 use Modules\React\Services\ReactService;
 use Tests\TestCase;
@@ -15,6 +20,8 @@ class ArticleCountsTest extends TestCase
 
     public function test_article_likes_use_count_model(): void
     {
+        Event::fake();
+
         $user    = User::factory()->create();
         $article = Article::factory()->create();
 
@@ -27,29 +34,21 @@ class ArticleCountsTest extends TestCase
 
         $article->like($user->id);
 
-        $this->assertDatabaseHas('counts', [
-            'countable_type' => $article->getMorphClass(),
-            'countable_id'   => $article->id,
-            'filter'         => 'likes',
-            'count'          => 1,
-        ]);
+        Event::assertDispatched(UserLikedEvent::class);
 
         $this->assertEquals(1, $article->likesCount());
 
         $article->unlike($user->id);
 
-        $this->assertDatabaseHas('counts', [
-            'countable_type' => $article->getMorphClass(),
-            'countable_id'   => $article->id,
-            'filter'         => 'likes',
-            'count'          => 0,
-        ]);
+        Event::assertDispatched(UserUnlikedEvent::class);
 
         $this->assertEquals(0, $article->likesCount());
     }
 
     public function test_article_dislikes_use_count_model(): void
     {
+        Event::fake();
+
         $user    = User::factory()->create();
         $article = Article::factory()->create();
 
@@ -57,23 +56,15 @@ class ArticleCountsTest extends TestCase
 
         $article->dislike($user->id);
 
-        $this->assertDatabaseHas('counts', [
-            'countable_type' => $article->getMorphClass(),
-            'countable_id'   => $article->id,
-            'filter'         => 'dislikes',
-            'count'          => 1,
-        ]);
+        Event::assertDispatched(UserDislikedEvent::class);
 
         $this->assertEquals(1, $article->dislikesCount());
 
         $article->undislike($user->id);
 
-        $this->assertDatabaseHas('counts', [
-            'countable_type' => $article->getMorphClass(),
-            'countable_id'   => $article->id,
-            'filter'         => 'dislikes',
-            'count'          => 0,
-        ]);
+        Event::assertDispatched(UserUndislikedEvent::class);
+
+        $this->assertEquals(0, $article->dislikesCount());
     }
 
     public function test_article_comments_use_count_model(): void
@@ -92,7 +83,7 @@ class ArticleCountsTest extends TestCase
 
         $this->assertEquals(1, $article->commentsCount());
 
-        $reply = app(ReactService::class)->insertComment($article->getMorphClass(), $article->id, 'Test Reply Content', $user->id, $comment->id);
+        app(ReactService::class)->insertComment($article->getMorphClass(), $article->id, 'Test Reply Content', $user->id, $comment->id);
 
         $this->assertDatabaseHas('counts', [
             'countable_type' => $article->getMorphClass(),
