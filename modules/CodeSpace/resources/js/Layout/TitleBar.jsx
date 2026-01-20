ed412054bea96bbb23c724f9d81a628f25f4f634
Ömer Faruk Demirel <omerfdmrl@gmail.com>
2026-01-20 12:07:27 +0300

    refactor(codespace): use inertia link to close codespace

--- modules/CodeSpace/resources/js/Layout/TitleBar.jsx
+++ modules/CodeSpace/resources/js/Layout/TitleBar.jsx
@@ -10,6 +10,7 @@ import { Button } from "@/Components/Ui/Form/Button";
 import { Menu, X } from "lucide-react";
 import { useTranslation } from "react-i18next";
 import { useSiteStore } from "@/Stores/SiteStore";
+import { Link } from "@inertiajs/react";
 
 export default function TitleBar() {
 	const { setQuickOpen, toggleSidebar, togglePanel, layout, activeFile, name } = useEditorStore();
@@ -177,15 +178,16 @@ export default function TitleBar() {
 			</div>
 
 			<div className="flex items-center">
-				<Button
-					variant="ghost"
-					size="sm"
-					className="h-9 w-12 rounded-none hover:bg-red-500 hover:text-white"
-					onClick={() => (window.location.href = "/")}
-					title="Close Editor"
-				>
-					<X size={16} />
-				</Button>
+				<Link href={route("home.view")}>
+					<Button
+						variant="ghost"
+						size="sm"
+						className="h-9 w-12 rounded-none hover:bg-red-500 hover:text-white"
+						title="Close Editor"
+					>
+						<X size={16} />
+					</Button>
+				</Link>
 			</div>
 		</div>
 	);
