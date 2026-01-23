3acfc12cf10aadbaa24b8114949ad68ec9d55db2
Ömer Faruk Demirel <omerfdmrl@gmail.com>
2026-01-23 18:55:17 +0300

    refactor(entry): use actions in crud controller

---
+++ modules/Entry/app/Actions/CreateEntryAction.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Modules\Entry\Actions;
+
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
+use Modules\Entry\Models\Entry;
+
+class CreateEntryAction
+{
+    public function execute($data): Entry
+    {
+        return DB::transaction(function () use ($data) {
+            $entry = Entry::create([
+                'slug'    => Str::uuid()->toString(),
+                'content' => $data['content'],
+                'user_id' => Auth::id(),
+            ]);
+
+            if (isset($data['media'])) {
+                $entry->addMultipleMediaFromRequest(['media'])
+                    ->each(function ($fileAdder) {
+                        $fileAdder->toMediaCollection('content');
+                    });
+            }
+
+            return $entry;
+        });
+    }
+}
