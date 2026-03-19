3e71fc4dfa039abf43427c16adc00e00b7c7de41
Ömer Faruk Demirel <omerfdmrl@gmail.com>
2026-03-19 20:46:04 +0300

    ci: remove cloudflare adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,10 @@ import UnoCSS from 'unocss/astro'
 import icon from 'astro-icon'
 import sitemap from '@astrojs/sitemap'
 import partytown from '@astrojs/partytown'
-import cloudflare from '@astrojs/cloudflare'
 
 export default defineConfig({
 	site: 'https://4byte.dev',
 	output: 'static',
-	adapter: cloudflare(),
 	integrations: [
 		UnoCSS(),
 		icon(),
