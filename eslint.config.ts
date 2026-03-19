71e89caf0a1269b9bcd376e88f0851bebcd7a5fe
Ömer Faruk Demirel <omerfdmrl@gmail.com>
2026-03-19 20:04:19 +0300

    lint: eslint configured

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,10 +1,26 @@
 import js from '@eslint/js'
 import astro from 'eslint-plugin-astro'
+import globals from 'globals'
 
 export default [
-  {
-    ignores: ['dist/**', '.astro/**', 'node_modules/**']
-  },
-  js.configs.recommended,
-  ...astro.configs.recommended
+	{
+		ignores: ['dist/**', '.astro/**', 'node_modules/**'],
+	},
+	{
+		files: ['*.astro'],
+		parser: 'astro-eslint-parser',
+		parserOptions: {
+			parser: '@typescript-eslint/parser',
+		},
+		extends: ['plugin:astro/recommended'],
+	},
+	{
+		files: ['scripts/**/*.mjs', 'scripts/**/*.js'],
+		languageOptions: {
+			globals: {
+				...globals.node,
+			},
+		},
+	},
+	js.configs.recommended,
 ]
